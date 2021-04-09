018d76f0325ac54593be4d46522ddbb3b63ae939
Toby Inkster <mail@tobyinkster.co.uk>
2021-04-09 10:16:59 +0100

    Document LINQ::Util::Internal

--- lib/LINQ/Util/Internal.pm
+++ lib/LINQ/Util/Internal.pm
@@ -55,3 +55,49 @@ sub assert_code {
 } #/ sub assert_code
 
 1;
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+LINQ::Util::Internal - common functions used internally by LINQ
+
+=head1 FUNCTIONS
+
+=over
+
+=item C<< create_linq( SOURCE ) >>
+
+=item C<< throw( EXCEPTION_TYPE, ARGS ) >>
+
+=item C<< assert_code( CALLABLE ) >>
+
+=back
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=LINQ>.
+
+=head1 SEE ALSO
+
+L<LINQ>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2021 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
