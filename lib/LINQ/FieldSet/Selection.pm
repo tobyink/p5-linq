f83111e1a0166f59ed054127b6481c18db95973e
Toby Inkster <mail@tobyinkster.co.uk>
2021-04-19 02:06:04 +0100

    bump version; changelog

--- lib/LINQ/FieldSet/Selection.pm
+++ lib/LINQ/FieldSet/Selection.pm
@@ -5,7 +5,7 @@ use warnings;
 package LINQ::FieldSet::Selection;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use Class::Tiny;
 use parent qw( LINQ::FieldSet );
--- lib/LINQ/Util.pm
+++ lib/LINQ/Util.pm
@@ -5,7 +5,7 @@ use warnings;
 package LINQ::Util;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use Exporter::Shiny qw( field fields check_fields );
 
